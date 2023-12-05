68de51a212fa33ecec49e9e606c5e87b2924523f
FrankAaron <2741218588@qq.com>
2023-12-05 17:35:26 +0800

    竞赛种花题
    过啦

--- grow _flowers.py
+++ grow _flowers.py
@@ -1,14 +1,14 @@
 def fection(flowers,n,count):
     l = len(flowers)
-    if flowers[0] == 1:
+    if flowers[0] == "1":
         for i in range(1,l-1):
-            if flowers[i-1] == flowers[i+1] == 0 and flowers[i] != 1:
-                flowers[i] = 1
+            if flowers[i-1] == flowers[i+1] == "0" and flowers[i] != "1":
+                flowers[i] = "1"
                 count += 1
-        if flowers[-1] == 0 and flowers[-2] == 0:
+        if flowers[-1] == "0" and flowers[-2] == "0":
                 count += 1
-    elif flowers[0] == 0:
-        flowers[0] = 1
+    elif flowers[0] == "0":
+        flowers[0] = "1"
         count += 1
         n = n
         return fection(flowers,n,count) 
