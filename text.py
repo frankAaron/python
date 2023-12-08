04e86ead78929b0b1df1c11f13c9e913715e6171
FrankAaron <2741218588@qq.com>
2023-12-08 22:29:20 +0800

    Update text.py

--- text.py
+++ text.py
@@ -1,26 +1,6 @@
-def count(a):
-    if a%2 == 0:
-        return a//2-1
-    else:
-        return (a-1)//2
-
-l = input()
-n = int(l[-1])
-s=l[l.find('[')+1:l.find(']')].split(',')
-sum = 0
-a = 0
-if s.count('1'):
-    b = s.index('1')
-    for i in s[b:]:
-        if i == '0':
-            a += 1
-        if i == '1':
-            sum += count(a)
-            a = 0
-    sum += (a+b)//2
-else:
-    sum = len(l)//2
-if sum >= n:
-    print("True")
-else:
-    print("False")
+for i in range(3,21):
+    for j in range(3,round(i**0.5)+1,2):
+        if i%j == 0:
+            break
+        else:
+            print(i)
