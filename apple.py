d1ae306abef8cdd4e27aa8d28830f9d19530e9b3
FrankAaron <2741218588@qq.com>
2024-02-01 16:42:44 +0800

    require

--- apple.py
+++ apple.py
@@ -1,7 +1,11 @@
-l = map(int,input().split())
-n = int(input())
+
+'''l = map(int,input().split())
+n = map(int,input())
 count = 0
 for i in l:
     if (n + 30) >= i:
         count += 1
-print(count)
+print(count)'''
+import text
+print(text.now)
+print(text.stu1.good('py'))
--- text.py
+++ text.py
@@ -1,6 +1,22 @@
-for i in range(3,21):
-    for j in range(3,round(i**0.5)+1,2):
-        if i%j == 0:
-            break
-        else:
-            print(i)
+def now():
+    print("2024")
+f = now
+f()
+class student:
+    def __init__(self,name,age): 
+        #私有
+        self.__name = name
+        self.__age = age
+    #获取并修改属性
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self,name):
+        self.name = name or '无名氏'
+
+    def good_stu(self,class_name):
+        print(f"正在学习{class_name}")
+stu1 = student('py',20)      
+print(stu1.good)
