238d33ddd4c47410434062146b42764810aec88d
FrankAaron <2741218588@qq.com>
2024-04-23 20:53:20 +0800

    Update try.py

--- try.py
+++ try.py
@@ -1,11 +1,13 @@
 # web应用程序
-import socket
-socket = socket.socket()  # 构建socket类
-socket.bind(("127.0.0.1", 8090))
-socket.listen(5)
-while 1:
-    conn, addr = socket.accept() #  阻塞等待客户
-    data = conn.recv(1024)
-    print("require:\n", data)
-    req = conn.send(b"HTTP/1.1 200 ok\r\nserver:aaron\r\n\r\nhello world")
-    conn.close()
+# import socket
+# socket = socket.socket()  # 构建socket类
+# socket.bind(("127.0.0.1", 8090))
+# socket.listen(5)
+# while 1:
+#     conn, addr = socket.accept() #  阻塞等待客户
+#     data = conn.recv(1024)
+#     print("require:\n", data)
+#     req = conn.send(b"HTTP/1.1 200 ok\r\nserver:aaron\r\n\r\nhello world")
+#     conn.close()
+a = [1, 2, 3, 4]
+print(a[:-3:-1])
